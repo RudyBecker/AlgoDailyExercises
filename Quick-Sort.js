6a5070ec73ced4453354e86688192ed19567aed5
Rudy Becker <52896529+RudyBecker@users.noreply.github.com>
2021-01-19 15:15:45 -0800

    Create Quick-Sort.js

---
+++ Quick-Sort.js
@@ -0,0 +1,22 @@
+// QUICK SORT
+
+let values = [50, 34, 21, 2, 3, 5, 7, 9];
+
+function quickSort(values) {
+  if (values.length <= 1) {
+    return values;
+  }
+
+  let lessThanPivot = [];
+  let greaterThanPivot = [];
+  const pivot = values.shift();
+
+  for (let i = 0; i < values.length; i++) {
+    const value = values[i];
+    value <= pivot ? lessThanPivot.push(value) : greaterThanPivot.push(value);
+  }
+
+  return [...quickSort(lessThanPivot), pivot, ...quickSort(greaterThanPivot)];
+}
+
+console.log(quickSort(values));
